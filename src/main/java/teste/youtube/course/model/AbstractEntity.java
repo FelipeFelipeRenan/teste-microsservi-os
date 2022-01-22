43873aec682334f8e45bb6e752fa1b8aeadf432d
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-22 18:28:44 -0300

    Fixing everything

--- src/main/java/teste/youtube/course/model/AbstractEntity.java
+++
@@ -1,7 +0,0 @@
-package teste.youtube.course.model;
-
-import java.io.Serializable;
-
-public interface AbstractEntity extends Serializable {
-    Long getId();
-}
--- src/main/java/teste/youtube/course/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package teste.youtube.course.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import teste.youtube.course.model.Course;
-
-public interface CourseRepository extends PagingAndSortingRepository<Course, Long>{
-}
