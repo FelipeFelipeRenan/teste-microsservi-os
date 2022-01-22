59e1c457c19698bbeea3583f494f28ec28a56eb1
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-22 15:41:33 -0300

    Fixing Pageable import

--- src/main/java/teste/youtube/course/endpoint/services/CourseService.java
+++ src/main/java/teste/youtube/course/endpoint/services/CourseService.java
@@ -3,11 +3,11 @@ package teste.youtube.course.endpoint.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import teste.youtube.course.model.Course;
 import teste.youtube.course.repository.CourseRepository;
 
-import java.awt.print.Pageable;
 
 @Service
 @Slf4j
